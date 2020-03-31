a7e5312557fd55d7923bd8cfea8a18de026390c5
Tirtharaj Dash <dashtirtharaj@gmail.com>
2020-03-31 18:36:51 +0530

    commit 1.1

--- displayNeighbors.m
+++ displayNeighbors.m
@@ -2,8 +2,9 @@
 % Inputs:  number of locations (N), nbd (a neighborhood set)
 % Outputs: None. Prints the neighbors for each location
 function displayNeighbors(N,nbd)
+fprintf('Vertex\t Neighbors\n');
 for i=1:N
-    fprintf('%d: ',i);
+    fprintf('%d\t',i);
     for j=1:length(nbd{i})
         fprintf('%d ',nbd{i}(j));
     end
